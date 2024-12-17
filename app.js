ac9ede3e56da50e9f0cf52cc338d06fc465f9c80
Barsoum Adly <barsoumadly@gmail.com>
2024-12-17 18:24:14 +0200

    main app

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 import express from "express";
 import cityRoutes from "./routes/cityRoutes.js";
 import connectToDatabase from "./database/connectToDatabase.js";
+import envVariables from "./config/envVariables.js";
 
 const app = express();
+const port = envVariables.PORT || 8000;
 
 app.use(express.json());
 
 app.use("/api", cityRoutes);
 
-app.listen(8080, async function () {
+app.listen(port, async function () {
   try {
     await connectToDatabase();
-    console.log("Sever is running on port 8080");
+    console.log(`Sever is running on port ${port}`);
   } catch (error) {
     console.log(error.message);
   }
