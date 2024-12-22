c562ce999c24578553a7f7f7970bda41eacb5091
Barsoum Adly <barsoumadly@gmail.com>
2024-12-22 20:19:58 +0200

    item controller

---
+++ controllers/itemController.js
@@ -0,0 +1,81 @@
+import Item from "../models/itemModel.js";
+
+const getItems = async function (request, response) {
+  try {
+    const items = await Item.find();
+    response.status(200).json({
+      status: "success",
+      result: items.length,
+      data: items,
+    });
+  } catch (error) {
+    response.status(404).json({
+      message: "No Items are found",
+    });
+  }
+};
+
+const createItem = async function (request, response) {
+  try {
+    const cityObj = request.body;
+    const newItem = await Item.create(cityObj);
+
+    response.status(201).json({
+      status: "success",
+      data: newItem,
+    });
+  } catch (error) {
+    response.status(400).json({
+      message: error.message,
+    });
+  }
+};
+
+const updateItem = async function (request, response) {
+  const id = request.params.id;
+  const cityObj = request.body;
+  try {
+    const Item = await Item.findByIdAndUpdate(id, cityObj);
+
+    response.status(200).json({
+      status: "success",
+      data: Item,
+    });
+  } catch (error) {
+    response.status(404).json({
+      message: "No Item found with that id",
+    });
+  }
+};
+
+const deleteItem = async function (request, response) {
+  const id = request.params.id;
+
+  try {
+    await Item.findByIdAndDelete(id);
+
+    response.status(204).json({
+      data: null,
+    });
+  } catch (error) {
+    response.status(404).json({
+      message: "No Item found with that id",
+    });
+  }
+};
+
+const deleteItems = async function (request, response) {
+  try {
+    await Item.deleteMany();
+
+    response.status(204).json({
+      data: null,
+    });
+  } catch (error) {
+    response.status(404).json({
+      message: "No Items found",
+    });
+  }
+};
+
+export { getItems, createItem, updateItem, deleteItem, deleteItems };
