81ec391e2ab7a30f4073dbba64b12b378c93d3ae
Barsoum Adly <barsoumadly@gmail.com>
2024-12-22 20:19:52 +0200

    main app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cityRoutes from "./routes/cityRoutes.js";
+import itemRoutes from "./routes/itemRoutes.js";
 import connectToDatabase from "./database/connectToDatabase.js";
 import envVariables from "./config/envVariables.js";
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api", cityRoutes);
+app.use("/api", itemRoutes);
 
 app.listen(port, async function () {
   try {
